a31dc0aaf48db00bf2a508feaaca106e0994ac9d
forreal20 <farhidyathuman@gmail.com>
2025-09-01 19:58:13 +0300

    Today my practise base on Canva tool were i mostly start creat a simlpe design so far i keep going on it, but in sigma i stuck apart and lastly i added new file named simpleExe.js which include JS code for dayly practise

---
+++ All-project/simpleExe.js
@@ -0,0 +1,10 @@
+let int;
+let string;
+let yourAge=18;
+console.log("Enter your name");
+console.log("Enter your age");
+if(yourAge<18){
+    console.log("you are not allowed to enter");
+}else{
+    console.log("your Qualfied to enter");
+}
