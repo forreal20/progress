a1736116698805e1075b1470d75e1ef6049dc3ed
forreal20 <farhidyathuman@gmail.com>
2025-09-03 19:58:35 +0300

    Today my practise based on JS where i continue with Array in JS  progres via codeacademy

--- All-project/newProject.js
+++ All-project/newProject.js
@@ -45,3 +45,34 @@ switch (athleteFinalPosition) {
   console.log('No medal awarded.');
   break;
 }
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+chores.push('wash dishes', 'do laundry');
+console.log(chores);
+chores.pop();
+console.log(chores);
+chores.shift();
+console.log(chores);
+chores.unshift('take out trash');
+console.log(chores);
+
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+groceryList.splice(4,1,'avocado','grapes');
+console.log(groceryList);
+const pastaIndex = groceryList.indexOf('pasta');
+console.log(pastaIndex);
+
+const concept = ['arrays', 'can', 'be', 'mutated'];
+function changeArr(arr) {
+  arr[3] = 'MUTATED';
+}
+changeArr(concept);
+console.log(concept);
+
+const removeElement = newArr => {
+  newArr.pop();
+}
+removeElement(concept);
+console.log(concept);
+
+let numberClusters = [[1,2], [3,4], [5,6]];
+console.log(numberClusters[2][1]);
